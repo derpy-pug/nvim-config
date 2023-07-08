d46472bb1834809a7b9cd9f7a29fcac5f826c512
Ethan Lynch <ethanwlynch17@gmail.com>
2023-07-08 18:25:19 -0500

    Removed nvim binary and added copilot

--- lua/derpypug/packer.lua
+++ lua/derpypug/packer.lua
@@ -44,6 +44,7 @@ return require('packer').startup(function(use)
       {'L3MON4D3/LuaSnip'},     -- Required
     }
   })
+  use('github/copilot.vim')
   use('nvim-treesitter/nvim-treesitter-context') 
   use('nvim-tree/nvim-web-devicons')
   use {
